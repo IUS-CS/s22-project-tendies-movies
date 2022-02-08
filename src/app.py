c50c8fb5893db5078a25ea0d0b540a30c0634f51
SanctusStercus <62039470+SanctusStercus@users.noreply.github.com>
2022-02-07 19:26:15 -0500

    started prototype for website

--- src/app.py
+++ src/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+pictures = os.path.join('static','pics')
+
+app.config['UPLOAD_FOLDER'] = pictures
 
 # defining a route
 @app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
 def home(): # route handler function
     # returning a response
-    return render_template('index.html')
+    tempPic = os.path.join(app.config['UPLOAD_FOLDER'], 'pic1.jpg' )
+    return render_template('index.html', user_image = tempPic)
+
 
 #debug mode on
 app.run(debug = True)
