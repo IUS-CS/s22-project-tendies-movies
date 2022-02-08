5ee52f60d42e3240fd24b1a163e74ed0cf949887
SanctusStercus <62039470+SanctusStercus@users.noreply.github.com>
2022-02-08 18:57:25 -0500

    finished a working prototype

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-pictures = os.path.join('static','pics')
+pictures = os.path.join('static','pics') #load pictures folder to flask
 
 app.config['UPLOAD_FOLDER'] = pictures
 
@@ -11,9 +11,16 @@ app.config['UPLOAD_FOLDER'] = pictures
 @app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
 def home(): # route handler function
     # returning a response
-    tempPic = os.path.join(app.config['UPLOAD_FOLDER'], 'pic1.jpg' )
-    backPic = os.path.join(app.config['UPLOAD_FOLDER'], 'background.jpg' )
-    return render_template('index.html', user_image = tempPic, user_image1=backPic)
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'THING1':
+            print ("THING1")
+        elif request.form['submit_button'] == 'THING2':
+            print ("THING2")
+        else:
+            pass # unknown
+    tempPic = os.path.join(app.config['UPLOAD_FOLDER'], 'pic1.jpg' ) #temp variable for sample pic
+    backPic = os.path.join(app.config['UPLOAD_FOLDER'], 'background.jpg' ) #variable for background image
+    return render_template('index.html', user_image = tempPic, user_image1=backPic) #return html, sample pic, and background picture
 
 
 #debug mode on
