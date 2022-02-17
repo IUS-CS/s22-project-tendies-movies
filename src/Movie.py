5fc120ae4909de3654fbb63a54577883756e6005
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2022-02-17 14:45:49 -0500

    Added Movie class

---
+++ src/Movie.py
@@ -0,0 +1,10 @@
+class Movie:
+    #Basic constructor for movie elements
+    def __init__(self):
+        self.id = 0
+        self.title = ""
+        self.description = ""
+        self.image = ""
+        self.genres = []
+        self.imdb_rating = 0
+        self.stars = 0
