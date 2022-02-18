cdd90962aea9c2800447b737f364489096c90138
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2022-02-17 21:14:53 -0500

    Movie class and test list

--- src/app.py
+++ src/app.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template, request
+from Movie import Movie
 import os
 
 app = Flask(__name__)
-
 pictures = os.path.join('static','pics') #load pictures folder to flask
-
 app.config['UPLOAD_FOLDER'] = pictures
+
+#list of movies, WIP TESTING
+movies = []
+movies.append(Movie())
+movies[0].name = "test"
+movies[0].image = os.path.join(app.config['UPLOAD_FOLDER'], 'movietest.jpg')
 
 # defining a route
 @app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
 def home(): # route handler function
-    # returning a response
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'THING1':
-            print ("THING1")
-        elif request.form['submit_button'] == 'THING2':
-            print ("THING2")
-        else:
-            pass # unknown
     tempPic = os.path.join(app.config['UPLOAD_FOLDER'], 'pic1.jpg' ) #temp variable for sample pic
     backPic = os.path.join(app.config['UPLOAD_FOLDER'], 'background.jpg' ) #variable for background image
+
+    # Button handling
+    if request.method == 'POST':
+        #testing button return
+        return render_template('index.html', user_image = movies[0].image, user_image1 = backPic)
+    
     return render_template('index.html', user_image = tempPic, user_image1=backPic) #return html, sample pic, and background picture
+
 
 
 #debug mode on
