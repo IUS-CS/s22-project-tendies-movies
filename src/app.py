12292f56c20d6438604bc96a35f86a8904e0523e
James Wright <jameswright@Jamess-MacBook-Pro-2.local>
2022-02-22 18:48:58 -0500

    added movie image via url, plot summary, and genres for yes button as well as minor ui tinkering

--- src/app.py
+++ src/app.py
@@ -13,6 +13,22 @@ movie = ia.get_top250_movies()
 random_Movie=randint(0,249)
 title = movie[random_Movie]['title']
 rating = movie[random_Movie]['rating']
+# specific id number to retrieve movie object from cinemagoer
+id = movie[random_Movie].getID()
+#cinemagoer specific movie object with access to more data fields (like cover url, plot summary, genres, etc)
+cinemagoer_movie_object = ia.get_movie(id)
+url = cinemagoer_movie_object['full-size cover url']
+#TODO fix formatting
+plot_summary = cinemagoer_movie_object['plot summary']
+#TODO fix formatting
+genres = cinemagoer_movie_object['genres']
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 pictures = os.path.join('static','pics') #load pictures folder to flask
@@ -28,9 +44,12 @@ def home(): # route handler function
     # Button handling
     if request.method == 'POST':
         #testing button return
-        return render_template('yes.html', movie_title=title, movie_rating = rating, user_image = tempPic, yes_background = yesbackPic)
+        if request.form['action'] == 'Yes!':
+            return render_template('yes.html', movie_title=title, movie_rating = rating, user_image = url, yes_background = yesbackPic , movie_plot_summary = plot_summary, movie_genres = genres)
+        if request.form['action'] == 'No.':
+            return render_template('no.html', movie_title=title, movie_rating = rating, user_image = url, background=backPic)
     
-    return render_template('index.html', movie_title=title, movie_rating = rating, movie_poster = tempPic, background=backPic) #return html, sample pic, and background picture
+    return render_template('index.html', movie_title=title, movie_rating = rating, user_image = url, background=backPic) #return html, sample pic, and background picture
 
 
 
