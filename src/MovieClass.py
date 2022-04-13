277b65ff8628ff316b6f50a6fe9b3d785b9e2ee9
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2022-04-13 04:47:42 -0400

    Added support for genre selection

--- src/MovieClass.py
+++ src/MovieClass.py
@@ -7,38 +7,46 @@ class MovieClass:
         self.movies = []
         self.index = 0
         self.cinemagoer = Cinemagoer()
+        self.movie_obj = None
 
     # Get top 250 movies
     def import_top250(self):
         self.movies = self.cinemagoer.get_top250_movies()
 
-    # Import a list of movies into the class
-    def add(self, movie_list):
-        self.movies = movie_list
+    # Takes a list of genres and removes all movies that are not
+    # any of those genres
+    def select_genre(self, genres):
+        self.movies = self.cinemagoer.get_top50_movies_by_genres(genres)
         self.index = 0
-
+        
     # Append a movie to the movie list
     def append(self, movie):
         self.movies.append(movie)
 
     # Increment to the next movie in the list
     def next(self):
+        #Set movie_obj to null
+        self.movie_obj = None
+
         #if we don't go out of bounds, increment
         if self.index < len(self.movies):
             self.index += 1
         else:
-        #otherwise, wrap around to 0
-            self.index = 0
+            raise Exception("Movie list is out of bounds.")
+        #otherwise, just stay at the end of the list
 
     # Return the movie object of the current movie
     def movie_object(self):
-        id = self.movies[self.index].getID()
-        movie_object = self.cinemagoer.get_movie(id)
-        return movie_object
+        #If there is not a movie object for the current film, make one
+        if self.movie_obj == None:
+            id = self.movies[self.index].getID()
+            self.movie_obj = self.cinemagoer.get_movie(id)
+        return self.movie_obj
 
     # Randomize the internal movie list
     def randomize(self):
         random.shuffle(self.movies)
+        self._verify_genre()
 
     # Return the title of the current movie
     def title(self):
@@ -46,7 +54,7 @@ class MovieClass:
 
     # Return the current movie rating
     def rating(self):
-        return self.movies[self.index]["rating"]
+        return self.movie_object()["rating"]
 
     # Return the URL for the cover image of the current movie
     def cover_url(self):
@@ -56,4 +64,7 @@ class MovieClass:
         return self.movie_object()["plot summary"]
 
     def genres(self):
+        #We don't actually need to go create a movie object
+        #for the genres, since we included it in the default information
+        #set on line 6
         return self.movie_object()["genres"] 
